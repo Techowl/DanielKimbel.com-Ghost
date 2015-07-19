b679fc1ff6b775af0c5b8d7dbe43f8d398e22d3b
dkimbel-lendup <daniel.kimbel@lendup.com>
2015-07-18 18:17:46 -0700

    Set up skeletal js, include jQuery, lodash

---
+++ content/themes/archangel/assets/js/index.js
@@ -0,0 +1,31 @@
+var color = (function() {
+
+  // 'private' variables
+  //
+  var _schemeNames = [];
+  var _interval;
+
+  // 'private' functions
+  //
+  var _parseSchemeName = function(unparsedName) {};
+  var _setInterval = function(intervalMilliseconds) {};
+  var _clearInterval = function() {};
+
+  // 'public' functions
+  //
+  return {
+    set: function() {
+
+    },
+
+    rotate: function() {
+
+    },
+
+    logExplanation: function() {
+      console.log("Welcome to DanielKimbel.com!");
+    }
+  }
+}());
+
+// on document ready, call color.logExplanation
